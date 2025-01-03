9ef81f46c4b7407874d75dfc977738996298a5cd
masterchris101 <c.novelleruddy@bhcc.edu>
2025-01-03 09:25:31 -0500

    Initial Commit

---
+++ MatrixCodeProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MatrixCodeProject
+//
+//  Created by Cristian Novelle Ruddy on 1/3/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
